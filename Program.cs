a413b96e553c3092a5974c671e73c7e5968a5dd2
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2025-01-30 11:04:06 +0900

    入力フォームを作成しました

--- Program.cs
+++ Program.cs
@@ -10,7 +10,34 @@ namespace ConsoleTrump
     {
         static void Main(string[] args)
         {
+            int[] card = new int[4];
 
+            for (int i = 0; i < card.Length; i++)
+            {
+                while(true)
+                {
+                    Console.WriteLine("1～4の数字を入力してね");
+                    card[i]=int.Parse(Console.ReadLine());
+
+                    if(card[i]>=1&&card[i]<=4)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("無効な値です");
+                        Console.ReadLine();
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < card.Length; i++)
+            {
+                Console.WriteLine($"{card[i]}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
