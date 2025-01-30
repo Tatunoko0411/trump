9f824e3cdb40ea0b6e3e8f81a1ee114bd4ac5d18
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2025-01-30 12:03:24 +0900

    繰り返し遊べるようにしました

--- Program.cs
+++ Program.cs
@@ -14,72 +14,83 @@ namespace ConsoleTrump
             int[] number = new int[5];
             int pair = 0;
             int ThreeFour = 0;
-            for (int i = 0; i < card.Length; i++)
+            while (true)
             {
-                while(true)
-                {
-                    bool parse;
-                    string input;
-                    Console.WriteLine("1～4の数字を入力してね");
-                    input = Console.ReadLine();
-                   parse = int.TryParse(input,out card[i]);
-                    
 
-                    if(card[i]>=1&&card[i]<=4&&parse==true)
+                for (int i = 0; i < card.Length; i++)
+                {
+                    while (true)
                     {
-                        break;
+                        bool parse;
+                        string input;
+                        Console.WriteLine("1～4の数字を入力してね");
+                        input = Console.ReadLine();
+                        parse = int.TryParse(input, out card[i]);
+
+
+                        if (card[i] >= 1 && card[i] <= 4 && parse == true)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("無効な値です");
+                            Console.ReadLine();
+                        }
+
+                    }
+                }
+
+                for (int i = 0; i < card.Length; i++)
+                {
+                    Console.Write($"{card[i]}");
+                }
+
+                for (int i = 0; i < card.Length; i++)
+                {
+                    number[card[i]]++;
+                    if (number[card[i]] == 2)
+                    {
+                        pair++;
+                    }
+                    if (number[card[i]] >= 3)
+                    {
+                        ThreeFour = number[card[i]];
+                    }
+                }
+
+                if (pair >= 1)
+                {
+                    if (pair >= 2)
+                    {
+                        Console.WriteLine($"ツーペア");
+                    }
+                    else if (ThreeFour == 3)
+                    {
+                        Console.WriteLine($"スリーカード");
+                    }
+                    else if (ThreeFour == 4)
+                    {
+                        Console.WriteLine($"フォーカード");
                     }
                     else
                     {
-                        Console.WriteLine("無効な値です");
-                        Console.ReadLine();
+                        Console.WriteLine($"ワンペア");
                     }
-
-                }
-            }
-
-            for (int i = 0; i < card.Length; i++)
-            {
-                Console.Write($"{card[i]}");
-            }
-
-            for (int i = 0; i < card.Length; i++)
-            {
-                number[card[i]]++;
-                if (number[card[i]] == 2)
-                {
-                    pair++;
-                }
-                if(number[card[i]]>=3)
-                {
-                    ThreeFour = number[card[i]];
-                }
-            }
-
-            if(pair >= 1)
-            {
-                if (pair >= 2)
-                {
-                    Console.WriteLine($"ツーペア");
-                }
-                else if(ThreeFour==3)
-                {
-                    Console.WriteLine($"スリーカード");
-                }
-                else if (ThreeFour == 4)
-                {
-                    Console.WriteLine($"フォーカード");
                 }
                 else
                 {
-                    Console.WriteLine($"ワンペア");
+                    Console.WriteLine($"ノーペア");
+                }
+
+                Console.WriteLine($"終了しますか?y/n");
+                string fin = Console.ReadLine();
+                if(fin == "y"||fin =="Y")
+                {
+                    break;
                 }
             }
-            else
-            {
-                Console.WriteLine($"ノーペア");
-            }
-                Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
